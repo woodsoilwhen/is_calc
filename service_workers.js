bf6868c3f9ec36ddd31298be2b7796ca4deba3e7
woodsoilwhen <when@woodsoil.xyz>
2023-03-29 06:03:15 +0800

    udate

--- service_workers.js
+++ service_workers.js
@@ -1,22 +1,42 @@
-// var cacheName = "is_calc_v1.1"
-// var appShellFiles = [
-// "/icon/icon192.png"
-// ];
+var cacheName = "is_calc_v1.1"
+var appShellFiles = [
+  "/index.html",
+  "/index.js",
+  "/A2HS.js",
+  "/is_calc.webmanifest",
+  "/css/font-awwsome.css",
+  "/css/index.css",
+  "/fonts/fontawesome-webfont.eot",
+  "/fonts/fontawesome-webfont.ttf",
+  "/fonts/fontawesome-webfont.woff",
+  "/fonts/fontawesome-webfont.woff2",
+  "/fonts/fontawesome-webfont_1.eot",
+  "/fonts/glyphicons-halflings-regular.eot",
+  "/fonts/glyphicons-halflings-regular.ttf",
+  "/fonts/glyphicons-halflings-regular.woff",
+  "/fonts/glyphicons-halflings-regular.woff2",
+  "/icon/icon192.png",
+  "/images/bilibili.png",
+  "/css/font-awesome.css",
+  "/is_calc/index.html",
+  "/is_calc/is_calc.css",
+  "/is_calc/is_calc.js",
+  "/pr_calc/index.html",
+  "/pr_calc/pr_calc.css",
+  "/pr_calc/pr_calc.js"
+];
 
-// //安装
-// self.addEventListener('install',
-//   function (e) {
-//     console.log("[Service Worker] 安装");
-//     e.waitUntil(
-//       caches.open(cacheName).then(
-//         function(cache) {
-//           console.log("[Service Worker] 全部缓存：应用脚本和内容");
-//           cache.addAll(appShellFiles);
-//         }
-//       )
-//     );
-//   }
-// );
+//安装
+self.addEventListener('install',function (e) {
+  console.log("[Service Worker] 安装");
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log("[Service Worker] 全部缓存：应用脚本和内容");
+      cache.addAll(appShellFiles);
+    })
+  );
+});
+
 // //响应请求
 // // self.addEventListener('fetch',
 // //   function (e) {
@@ -58,14 +78,6 @@
 //     );
 //   }
 // )
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('fox-store').then((cache) => cache.addAll([
-      
-    ])),
-  );
-});
 
 self.addEventListener('fetch', (e) => {
   console.log(e.request.url);
