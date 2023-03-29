7411bfcb867066104f58d1a776cc39e04ee6c457
woodsoilwhen <when@woodsoil.xyz>
2023-03-29 21:35:05 +0800

    更新 service worker

--- service_workers.js
+++ service_workers.js
@@ -37,51 +37,31 @@ self.addEventListener('install',function (e) {
   );
 });
 
-// //响应请求
-// // self.addEventListener('fetch',
-// //   function (e) {
-// //     e.respondWith(
-// //       caches.match(e.request).then(
-// //         function(r){
-// //           console.log("[Service Worker] 响应资源 "+e.request.url);
-// //           return r || fetch(e.request).then(
-// //             function(response){
-// //               return caches.open(cacheName).then(
-// //                 function(cache){
-// //                   console.log('[Service Worker] 缓存新资源: '+e.request.url);
-// //                   cache.put(e.request,response.clone());
-// //                   return response
-// //                 }
-// //               )
-// //             }
-// //           )
-// //         }
-// //       ),
-// //     );
-// //   }
-// // );
-// //缓存清理
-// self.addEventListener("activate",
-//   function (e) {
-//     e.waitUntil(
-//       caches.keys().then(
-//         function (keyList) {
-//           return Promise.all(keyList.map(
-//             function (key) {
-//               if (cacheName.indexOf(key) === -1) {
-//                 return caches.delete(key);
-//               }
-//             }
-//           ))
-//         }
-//       )
-//     );
-//   }
-// )
-
-self.addEventListener('fetch', (e) => {
-  console.log(e.request.url);
+// 响应请求
+self.addEventListener('fetch',function (e) {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
+    caches.match(e.request).then(function(r){
+      console.log("[Service Worker] 响应资源 "+e.request.url);
+      return r || fetch(e.request).then(function(response){
+        return caches.open(cacheName).then(function(cache){
+          console.log('[Service Worker] 缓存新资源: '+e.request.url);
+          cache.put(e.request,response.clone());
+          return response
+        })
+      })
+    }),
   );
 });
+
+//缓存清理
+self.addEventListener("activate",function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function (key) {
+        if (cacheName.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }))
+    })
+  );
+})
